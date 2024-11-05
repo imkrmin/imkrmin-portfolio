b6897f8e462951dbf033da2a2eccb9a7acfeab72
imkrmin <zum2n2@gmail.com>
2024-11-05 21:25:41 +0900

    Feat : FlipImg 컴포넌트 구현

---
+++ src/components/domain/about/FlipImg.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+
+const FlipImg = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  return (
+    <div
+      className={`relative transition-all duration-500 [transform-style:preserve-3d] ${
+        isFlipped ? "[transform:rotateY(180deg)]" : ""
+      }`}
+    >
+      <Image
+        src={`/images/${isFlipped ? "profileimg(2)" : "profileimg"}.jpg`}
+        alt="프로필 이미지"
+        width={400}
+        height={400}
+        className={`rounded-lg opacity-80 ${
+          isFlipped ? "[transform:rotateY(180deg)]" : ""
+        } `}
+      />
+      <div className="absolute bottom-2 left-2 flex justify-center items-center w-[40px] h-[40px] rounded-full bg-[#171717] hover:opacity-80 cursor-pointer">
+        <Image
+          src={`/icons/arrow-left-right.svg`}
+          alt="뒤집기 아이콘"
+          width={20}
+          height={20}
+          onClick={e => {
+            e.stopPropagation();
+            setIsFlipped(!isFlipped);
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FlipImg;
